92b7ed6f151b16b9427b574397739d8e1d5bb4a0
GingerBeardx <ecryptographer@gmail.com>
2021-03-31 19:20:31 -0600

    Added dependency injection for the CharacterController.

---
+++ WebRPG/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebRPG.Models;
+
+namespace WebRPG.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character character);
+    }
+}
--- WebRPG/Startup.cs
+++ WebRPG/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebRPG.Services.CharacterService;
 
 namespace WebRPG
 {
@@ -26,6 +27,7 @@ namespace WebRPG
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICharacterService, CharacterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

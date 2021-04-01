7900b4800b1746da0df4ac4eb0505204f384f9ec
GingerBeardx <ecryptographer@gmail.com>
2021-03-31 18:28:27 -0600

    Created Character Controller

---
+++ WebRPG/Controllers/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebRPG.Models;
+
+namespace WebRPG.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+    }
+}

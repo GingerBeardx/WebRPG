137c624344e49833ccdbbd5e5804b71a4b44c256
GingerBeardx <ecryptographer@gmail.com>
2021-03-31 19:48:31 -0600

    Added ServiceResponse Model and implemented in CharacterService

--- WebRPG/Services/CharacterService/ICharacterService.cs
+++ WebRPG/Services/CharacterService/ICharacterService.cs
@@ -8,8 +8,8 @@ namespace WebRPG.Services.CharacterService
 {
     public interface ICharacterService
     {
-        Task<List<Character>> GetAllCharacters();
-        Task<Character> GetCharacterById(int id);
-        Task<List<Character>> AddCharacter(Character character);
+        Task<ServiceResponse<List<Character>>> GetAllCharacters();
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character character);
     }
 }

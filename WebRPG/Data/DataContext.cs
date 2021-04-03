7894710c2783e1c809f42be6e2e3a3954eaad84d
GingerBeardx <ecryptographer@gmail.com>
2021-04-03 13:21:24 -0600

    Added User Model and migrated to database.

--- WebRPG/Data/DataContext.cs
+++ WebRPG/Data/DataContext.cs
@@ -9,11 +9,9 @@ namespace WebRPG.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-               
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }

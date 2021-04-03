3866340a9c96d02b3032965b0219fd992af68a87
GingerBeardx <ecryptographer@gmail.com>
2021-04-03 12:39:13 -0600

    Completed initial migration and initialized database.

--- WebRPG/Startup.cs
+++ WebRPG/Startup.cs
@@ -28,7 +28,7 @@ namespace WebRPG
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();

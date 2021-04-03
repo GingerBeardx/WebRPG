5402829bb6ecc85bb89bbce523dd224b5f809851
GingerBeardx <ecryptographer@gmail.com>
2021-04-03 17:23:56 -0600

    Modified Add, Update, and Delete character methods to only modify based on authorized user.

--- WebRPG/Services/CharacterService/ICharacterService.cs
+++ WebRPG/Services/CharacterService/ICharacterService.cs
@@ -9,7 +9,7 @@ namespace WebRPG.Services.CharacterService
 {
     public interface ICharacterService
     {
-        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters(int userId);
+        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto character);
         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
